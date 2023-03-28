fd0104c4b6d63fcea6e048fc5039fd7895e0a8b4
Neil Kakkar <neilkakkar@gmail.com>
2023-03-28 19:35:13 -0400

    inference base code

---
+++ inference.py
@@ -0,0 +1,13 @@
+import openai
+
+prompt = """
+
+
+"""
+openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+    ]
+)
