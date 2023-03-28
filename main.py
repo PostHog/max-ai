e54b0669612b93463014715a1310566abf0a7d8c
James Greenhill <fuziontech@gmail.com>
2023-03-28 20:42:02 +0000

    add slack bits

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+
+load_dotenv()  # take environment variables from .env.
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
---
+++ slack.py
@@ -0,0 +1,73 @@
+import os
+
+from dotenv import load_dotenv
+from slack_bolt import App
+
+
+load_dotenv()
+
+
+# Initializes your app with your bot token and signing secret
+app = App(
+        token=os.environ.get("SLACK_BOT_TOKEN"),
+        signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+
+# Add functionality here
+# @app.event("app_home_opened") etc
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
+
+
+# Start your app
+if __name__ == "__main__":
+        app.start(port=int(os.environ.get("PORT", 3000)))
+
